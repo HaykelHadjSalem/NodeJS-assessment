dc0ea3ec370d1b1de0ceaf16c06db559314035fc
HamdiBenK <hamdi@atayen.us>
2022-10-15 23:26:33 +0100

    add signin controller and column schema

--- middleware/passeport.middleware.js
+++ middleware/passeport.middleware.js
@@ -26,9 +26,8 @@ passport.use(
             const hashPassword = await bcrypt.hash(password, salt);
             let createdUser = await owner.create({email:username,password:hashPassword});
             let token = generateAccessToken(createdUser.toJSON())
-
             return done(null, {
-                id: createdUser._id,
+                userId: createdUser.id,
                 token,
             })
         }
@@ -64,3 +63,64 @@ exports.emailSignup = async (req, res, next) => {
  */
 
 
+/*
+ * begin signin with email and password
+ */
+
+const signinWithEmail = async (
+    req,
+    username,
+    password,
+    done,
+) => {
+    let user = await owner.findOne({
+        where: { email: username },
+      })
+      if(!user) return done(null, false, { message: 'user not found'})
+      const validPass = await bcrypt.compare(password, user.password);
+      if (!validPass) return done(null, false, { message: 'invalid password'})
+      let token = generateAccessToken(user.toJSON())
+      return done(null, {
+        userId: user.id,
+        token,
+    })
+}
+passport.use(
+    'signinEmailStrategy',
+    new LocalStrategy(
+        { passReqToCallback: true },
+        async (req, username, password, done) => {
+            await signinWithEmail(req, username, password, done)
+        }
+    )
+)
+exports.emailConnection = async (req, res, next) => {
+    passport.authenticate(
+        'signinEmailStrategy',
+        { session: false },
+        (err, user, info) => {
+            if (err) {
+                return responseHandler.makeResponseError(res, 401, err)
+            }
+            if (!user) {
+                return responseHandler.makeResponseError(res, 401, info)
+            }
+            req.logIn(user, function (err) {
+                var param = {
+                    access_token: user.token,
+                    id: user.id,
+                    ...user.message && {message: user.message} 
+                }
+                return responseHandler.makeResponseData(
+                    res,
+                    200,
+                    'success',
+                    param
+                )
+            })
+        }
+    )(req, res, next)
+}
+/*
+ * end signin with email and password
+ */
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {emailSignup} = require('../middleware/passeport.middleware')
+const {emailSignup,emailConnection} = require('../middleware/passeport.middleware')
 
 
 /**
@@ -31,5 +31,33 @@ const {emailSignup} = require('../middleware/passeport.middleware')
  */
  router.post('/signup', emailSignup)
 
+
+ /**
+ * @swagger
+ * /auth/signin:
+ *   post:
+ *     tags:
+ *     - "auth"
+ *     summary: signin with email and password.
+ *     description: user enter his credentials to login into the app <br> without access_token.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:      # Request body contents
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *          description: code,<br>message,<br>data:{"access_token":token,"id":user_id}
+ *       "401":
+ *          description: code,<br>error:{error:true,message:'account_already_used'}
+ *       "500":
+ *          description: error=eror
+ */
+ router.post('/signin', emailConnection)
 
  module.exports = router
